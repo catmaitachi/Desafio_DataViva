e9bfd16e8aa51209063664222f6e978f8f930e5b
Catmaitachi <lukasspiazzi@gmail.com>
2026-02-04 13:52:45 -0300

    feat: implementando algoritmos para os desafios 1 e 2

---
+++ codigos/FizzBuzz.py
@@ -0,0 +1,28 @@
+def fizzbuzz( incio , fim ):
+    
+    """
+    Desafio FizzBuzz: 
+    - Imprima os números dentro de um intervalo.
+    - Para múltiplos de 3, imprima "Fizz".
+    - Para múltiplos de 5, imprima "Buzz".
+    - Para múltiplos de ambos 3 e 5, imprima "FizzBuzz".
+
+    :param incio: (int) incio do intervalo 
+    :param fim: (int) final do intervalo
+    """
+
+    for i in range( incio, fim + 1 ): # Cria um loop no intervalo definido.
+
+        if i % 3 == 0 and i % 5 == 0: print( str(i) + " - FizzBuzz" ) # Verifica se o número é múltiplo de 3 e 5 ao mesmo tempo para imprimir "FizzBuzz".
+
+        elif i % 3 == 0: print( str(i) + " - Fizz" ) # Verifica se o número é múltiplo de 3 para imprimir "Fizz".
+
+        elif i % 5 == 0: print( str(i) + " - Buzz" ) # Verifica se o número é múltiplo de 5 para imprimir "Buzz".
+
+        else: print( str(i) ) # Imprime o número sem nada caso não seja múltiplo de 3 ou 5.
+
+    # ! Obs: Deixei a função sem validação de parâmetros para manter o foco no desafio proposto.
+
+    return None
+
+fizzbuzz( 1, 100 ) # Chama a função com o intervalo de 1 a 100.
---
+++ codigos/Palindromos.py
@@ -0,0 +1,28 @@
+def verificador_de_palindromo( palavra ):
+    
+    """
+    Desafio Verificador de Palíndromo: 
+    - Recebe uma palavra e verifica se é um palíndromo.
+
+    :param palavra: (str) Palavra a ser verificada.
+    :return: (bool) True se for palíndromo, False caso contrário.
+    """
+
+    palavra = palavra.lower() # Converte a palavra para minúsculas para garantir a comparação sem case sensitivity.
+    palavra_invertida = palavra[::-1] # Inverte a palavra usando slicing.
+
+    return palavra == palavra_invertida # Compara a palavra original com a invertida e retorna o resultado.
+
+def __main__():
+
+    palavras_teste = [ "Arara" , "Ovo" , "Casa" , "Biscoito" , "Scooby" ] # Lista de palavras para serem testadas.
+
+    for palavra in palavras_teste: # Loop através da lista de palavras.
+
+        if verificador_de_palindromo( palavra ): print( f'"{palavra}" é um palíndromo.' ) # Saída se for palíndromo.
+
+        else: print( f'"{palavra}" não é um palíndromo.' ) # Saída se não for palíndromo.
+
+    return None
+
+__main__()
